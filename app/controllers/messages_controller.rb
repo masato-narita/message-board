e31a05ad0a964d29d44f028b99b92cffb6a6ceb4
MasatoNarita <narita@digital-knowledge.co.jp>
2016-05-06 04:46:36 +0000

    add edit and destroy

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,7 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+
+# メッセージ一覧の表示  
   def index
     @message = Message.new
     # Messageを全て取得する。
@@ -16,6 +19,26 @@ class MessagesController < ApplicationController
       render 'index'
     end
   end
+
+# メッセージの編集
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+# メッセージの削除
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
   
 # ストロングパラメータ
   private
@@ -23,4 +46,7 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:name, :body)
   end
   ## ここまで
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
