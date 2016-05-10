1c9c946aa5bfcabefa2dded672c25c49d49d3562
MasatoNarita <narita@digital-knowledge.co.jp>
2016-05-10 04:31:38 +0000

    locales_ja

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,6 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
-  # 数字のみ入力可能。greater_than_or_equal_toはひとつのメソッド
+  # 数値のみ入力可能。greater_than_or_equal_toはひとつのメソッド
   validates :age, numericality: {greater_than_or_equal_to: 0}, presence: true
 end
